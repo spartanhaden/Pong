7c590a6c84606bd25fabea76f5055ba115fbf3a9
Haden Wasserbaech <spartanhaden@gmail.com>
2014-12-29 06:58:33 -0500

    Merged Robin's xml edits and added ball in single player view.

--- app/src/main/java/com/hadenw/pong/SinglePlayer.java
+++ app/src/main/java/com/hadenw/pong/SinglePlayer.java
@@ -9,6 +9,6 @@ public class SinglePlayer extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_single_player);
+		setContentView(new BallView(this));
 	}
 }
