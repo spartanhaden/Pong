14ba4757f58bcf925e819b0fe96fed4ce8caab71
Haden Wasserbaech <spartanhaden@gmail.com>
2014-12-29 06:10:17 -0500

    merged the menu from Robin with simple single player intent

--- app/src/main/java/com/hadenw/pong/FullscreenActivity.java
+++ app/src/main/java/com/hadenw/pong/FullscreenActivity.java
@@ -1,15 +1,29 @@
 package com.hadenw.pong;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
-public class FullscreenActivity extends Activity {
+public class FullscreenActivity extends Activity implements View.OnClickListener {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
 		setContentView(R.layout.activity_fullscreen);
+		final Button singlePlayerButton = (Button) findViewById(R.id.button_Single_Player);
+		singlePlayerButton.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch(v.getId()) {
+			case R.id.button_Single_Player:
+				Intent intent = new Intent(this, SinglePlayer.class);
+				startActivity(intent);
+				break;
+		}
 	}
 
 	public void onWindowFocusChanged(boolean hasFocus) {
